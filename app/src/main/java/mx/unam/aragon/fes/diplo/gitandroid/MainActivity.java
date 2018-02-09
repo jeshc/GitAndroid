ca79347edb4d556b47dd695f00fc0086f775d618
jeshc <hcjesus@gmail.com>
2018-02-09 09:24:06 -0600

    Se agrega boton y funcionalidad del mismo: Al dar clic el boton cambia el texto en el TextView respectivo

--- app/src/main/java/mx/unam/aragon/fes/diplo/gitandroid/MainActivity.java
+++ app/src/main/java/mx/unam/aragon/fes/diplo/gitandroid/MainActivity.java
@@ -2,12 +2,25 @@ package mx.unam.aragon.fes.diplo.gitandroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button btnMiBoton;
+    private TextView txtTexto;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnMiBoton=findViewById(R.id.button);
+        txtTexto=findViewById(R.id.textView);
+
+        btnMiBoton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                txtTexto.setText("Hola Github");
+            }
+        });
     }
 }
