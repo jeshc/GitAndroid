0e9199697fddfa74cdcc9de1a0f1ad4decb76512
jeshc <hcjesus@gmail.com>
2018-02-10 17:14:01 -0600

    Modificaciones de la clase de hoy

--- app/src/main/java/mx/unam/aragon/fes/diplo/gitandroid/MainActivity.java
+++ app/src/main/java/mx/unam/aragon/fes/diplo/gitandroid/MainActivity.java
@@ -9,18 +9,28 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
     private Button btnMiBoton;
     private TextView txtTexto;
+    private Button btnBoton2;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         btnMiBoton=findViewById(R.id.button);
         txtTexto=findViewById(R.id.textView);
+        btnBoton2=findViewById(R.id.button2);
 
         btnMiBoton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 txtTexto.setText("Hola Github");
 
+            }
+        });
+
+        btnBoton2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                txtTexto.setText("Diplomado Android");
             }
         });
     }
